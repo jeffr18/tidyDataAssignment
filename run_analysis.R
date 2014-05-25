d390c4ea45f0e49897ab067993359667897fee16
Jeff Rasmussen <github@digital-midnight.com>
2014-05-25 16:17:50 -0700

    updated documentation

--- run_analysis.R
+++ run_analysis.R
@@ -23,42 +23,78 @@ mergedTestData = cbind(xTestData, subjectTestData, mergedYTestData)
 finalData = rbind(mergedTrainData, mergedTestData)
 
 
+# Part 3 - Create human-readable variable names
+#convert periods to underscores
+names(finalData) <- gsub("...", "_", names(finalData), fixed=TRUE)
+names(finalData) <- gsub("..", "_", names(finalData), fixed=TRUE)
+names(finalData) <- gsub(".", "_", names(finalData), fixed=TRUE)
+
+#remove underscores that appear at the end
+names(finalData) <- gsub("_$", "", names(finalData))
+
+#replace text at beggining of variable name with more human readable text
+names(finalData) <- gsub("^tBody", "time_body", names(finalData))
+names(finalData) <- gsub("^fBody", "freq_body", names(finalData))
+names(finalData) <- gsub("^tGravity", "time_gravity", names(finalData))
+
+
 
 # Part 2 - Extract mean and standard deviation measurements
-# Issolate the mean and standard deviation values
+dataColumnNames <- c()
+
+# Create base data frame with id values
 issolatedData <- data.frame(subject_id=finalData$subject_id, activity_id=finalData$activity_id, activity_label=finalData$activity_label)
 
 # cbind mean columns.  Search column name for text "mean" but don't inlcude "meanFreq"
 meanColumns <- grepl("mean",names(finalData)) & !grepl("meanFreq", names(finalData))
+dataColumnNames <- c(dataColumnNames, names(finalData[meanColumns]))
 issolatedData <- cbind(issolatedData, finalData[meanColumns])
 
 # cbind standard deviation columns.  Search column name for text "std"
 stdColumns <- grepl("std",names(finalData))
+dataColumnNames <- c(dataColumnNames, names(finalData[stdColumns]))
 issolatedData <- cbind(issolatedData, finalData[stdColumns])
-
-# Part 3 - Create human-readable variable names
-#convert periods to underscores
-names(issolatedData) <- gsub("...", "_", names(issolatedData), fixed=TRUE)
-names(issolatedData) <- gsub("..", "_", names(issolatedData), fixed=TRUE)
-names(issolatedData) <- gsub(".", "_", names(issolatedData), fixed=TRUE)
-
-#remove underscores that appear at the end
-names(issolatedData) <- gsub("_$", "", names(issolatedData))
-
-#replace text at beggining of variable name with more human readable text
-names(issolatedData) <- gsub("^tBody", "time_body", names(issolatedData))
-names(issolatedData) <- gsub("^fBody", "freq_body", names(issolatedData))
-names(issolatedData) <- gsub("^tGravity", "time_gravity", names(issolatedData))
 
 
 # Part 4 - Creates a second, independent tidy data set with the average of each variable for each activity 
 # and each subject. 
+subjectList = unique(issolatedData$subject_id)
+activityList = unique(issolatedData$activity_label)
 
+tidyData <- data.frame()
 
+for (subjectValue in subjectList)
+{
+    for (activityValue in activityList)
+    {
+        # Create a subset for each subject and each activity
+        activityData <- subset(issolatedData, (issolatedData$subject_id == subjectValue) & 
+                                   (issolatedData$activity_label == activityValue))
+        
+        # loop through the data columns and generate the average for each one
+        for (columnValue in dataColumnNames)
+        {
+            # issolate the column for the specific data and get the average
+            dataColumn <- grepl(columnValue,names(activityData))
+            averageValue <- mean(activityData[,dataColumn])
+            
+            # create a record in the tidy data frame
+            tidyData <- rbind(tidyData, c(subjectValue, activityValue, columnValue, averageValue))
+        }
+    }
+}
+
+# Part 5 - Finalize tidy data and save to file
+# Set the column variable names for the tidy data frame
+names(tidyData) <- c("subject_id", "activity_label", "data_name", "average_value")
+
+# Convert subject_id and average_value to numeric
+tidyData$subject_id <- as.numeric(tidyData$subject_id)
+tidyData$average_value <- as.numeric(tidyData$average_value)
 
 # Write out tidy data to file
-outputFilePath = ./tidyData.txt
-write.table()
+outputFilePath <- "./tidy_data.txt"
+write.table(tidyData, file=outputFilePath, row.names=FALSE)
 
 
 
